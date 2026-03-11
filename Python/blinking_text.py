6d4e7beeaf4cfdc6e9a27fe05c650b3987112612
HD-git <knowledgelyf22@gmail.com>
2026-03-11 23:08:29 +1100

    blink-text-U1

---
+++ Python/blinking_text.py
@@ -0,0 +1,20 @@
+import time
+import os
+
+# Function to clear the screen
+def clear_screen():
+    os.system('clear' if os.name == 'posix' else 'cls')
+
+# The text to display
+text = "my heart blinks for you"
+text1 = "so what"
+
+# Main loop to make the text blink
+while True:
+    clear_screen()
+    print(text)
+    time.sleep(0.5)
+    print(text1)
+    time.sleep(0.10)
+    clear_screen()
+    time.sleep(0.5)
